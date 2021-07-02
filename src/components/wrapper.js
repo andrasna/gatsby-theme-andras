4d7bd7596b3e5eedc2399a304ac87605beed4feb
Andras Nagy <contact@andras.me>
2021-07-03 01:18:43 +0200

    Refactor function

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { normal, post } from '../styles/wrapper.module.css'
+import * as style from '../styles/wrapper.module.css'
 
 const Wrapper = ({ variant, children }) => {
-  if (variant === 'post') {
-    return (
-      <div className={post}>
-        {children}
-      </div>
-    )
-  }
-
   return (
-    <div className={normal}>
+    <div className={style[variant] ?? style.normal}>
       {children}
     </div>
   )
