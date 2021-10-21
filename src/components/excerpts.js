dba3850866fdade8de771a3853f52bb5a8ef6997
Andras Nagy <contact@andras.me>
2021-10-21 17:00:08 +0200

    Fix posts sort order

--- src/components/excerpts.js
+++ src/components/excerpts.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { useStaticQuery, graphql } from 'gatsby'
-import { excerpts, excerpt, title } from '../styles/excerpt.module.css'
+import { excerpts, excerpt, title, time } from '../styles/excerpt.module.css'
 import { card } from '../styles/card.module.css'
 
 const Excerpts = () => {
   const data = useStaticQuery(graphql`
     query PostExcerptsQuery {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
         nodes {
+          excerpt(pruneLength: 220)
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
           fields {
             slug
-          }
-          excerpt(pruneLength: 260)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
           }
         }
       }
@@ -30,7 +30,7 @@ const Excerpts = () => {
         return (
           <article key={i} className={`${card} ${excerpt}`}>
             <header>
-              <time dateTime={post.frontmatter.date}>
+              <time className={`${time}`} dateTime={post.frontmatter.date}>
                 {post.frontmatter.date}
               </time>
 
